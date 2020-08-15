968b87d08d0032ff31830a86710a795a96d7e569
César Escudero <cedaesca@gmail.com>
2020-08-15 08:56:15 -0400

    forcing json in all requests

---
+++ server/app/Http/Middleware/RequestsAcceptJson.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RequestsAcceptJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $acceptHeader = strtolower($request->headers->get('accept'));
+
+        if ($acceptHeader !== 'application/json') {
+            $request->headers->set('Accept', 'application/json');
+        }
+
+        return $next($request);
+    }
+}
