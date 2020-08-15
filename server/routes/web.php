6356e6664261d7a95366b10004b68e5e630eb414
César Escudero <cedaesca@gmail.com>
2020-08-15 12:09:56 -0400

    users route and controller added

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,3 +16,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'users'], function () use ($router) {
+    $router->post('/', 'UserController@store');
+});
