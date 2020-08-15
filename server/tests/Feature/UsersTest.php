c6d64b096a3aefd570380252166d178afe7376d2
César Escudero <cedaesca@gmail.com>
2020-08-15 13:48:01 -0400

    added test case for registration with invalid data

--- server/tests/Feature/UsersTest.php
+++ server/tests/Feature/UsersTest.php
@@ -22,6 +22,45 @@ class UsersTest extends TestCase
     }
 
     /** @test */
+    public function cannot_create_user_with_invalid_data()
+    {
+        // Testing with single attributes
+        $this->post('/users', [])->assertResponseStatus(422);
+        $this->post('/users', ['name' => 'Juanito'])->assertResponseStatus(422);
+        $this->post('/users', ['password' => '123456789'])->assertResponseStatus(422);
+        $this->post('/users', ['email' => 'john@example.com'])->assertResponseStatus(422);
+
+        // Testing in combination of two attributes
+        $this->post('/users', [
+            'name' => 'John Doe',
+            'email' => 'john@doe.com'
+        ])->assertResponseStatus(422);
+
+        $this->post('/users', [
+            'name' => 'John Doe',
+            'email' => 'john@doe.com'
+        ])->assertResponseStatus(422);
+
+        $this->post('/users', [
+            'email' => 'john@doe.com',
+            'password' => '123456789'
+        ])->assertResponseStatus(422);
+
+        // Testing with all attributes but invalid syntax
+        $this->post('/users', [
+            'name' => 'John Doe',
+            'password' => '12345',
+            'email' => 'john@doe.com'
+        ])->assertResponseStatus(422);
+
+        $this->post('/users', $user = [
+            'name' => 'John Doe',
+            'password' => '123456',
+            'email' => 'johndoecom'
+        ])->assertResponseStatus(422);
+    }
+
+    /** @test */
     public function successful_creation_returns_user_data()
     {
         $user = $this->getUnpersistedUser();
