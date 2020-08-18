fa6669d1a5633835693dad355306e87da16c65b5
César Escudero <cedaesca@gmail.com>
2020-08-18 17:03:50 -0400

    feat: index method returns authenticated user lists

--- server/app/Http/Controllers/ListController.php
+++ server/app/Http/Controllers/ListController.php
@@ -22,9 +22,9 @@ class ListController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return response()->json($request->user()->lists);
     }
 
     /**
