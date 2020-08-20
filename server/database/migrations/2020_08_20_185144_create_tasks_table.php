1ce85bd39c23a17435c11f21ba5533036e99bfc2
César Escudero <cedaesca@gmail.com>
2020-08-20 15:03:26 -0400

    feat: Task model addition with migration

---
+++ server/database/migrations/2020_08_20_185144_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('list_id')->constrained('tasks_lists')->onDelete('cascade');
+            $table->string('description');
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
