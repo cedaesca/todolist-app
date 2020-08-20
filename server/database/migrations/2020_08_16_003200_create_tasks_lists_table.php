386867bd9c5f2273c0b573d4f68962bb4692d4d8
César Escudero <cedaesca@gmail.com>
2020-08-20 14:02:38 -0400

    feat: removing soft deletes fields

--- server/database/migrations/2020_08_16_003200_create_tasks_lists_table.php
+++ server/database/migrations/2020_08_16_003200_create_tasks_lists_table.php
@@ -17,7 +17,6 @@ class CreateTasksListsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
