a96cf9c654c37d471c708cdce723e24c87f7515d
César Escudero <cedaesca@gmail.com>
2020-09-01 15:09:26 -0400

    fix: added fallback value for client_url

--- server/config/app.php
+++ server/config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'client_url' => env('APP_CLIENT_URL'),
+    'client_url' => env('APP_CLIENT_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
